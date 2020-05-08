21eb755bd1bba8ea9f477ed129f67d836eee95d2
FehSchiavon <felipschiavon@gmail.com>
2020-05-08 16:28:42 -0300

    chore: Usando Nunjucks para automacao e a criacao de novos objetos nas rotas do server.js

--- server.js
+++ server.js
@@ -2,21 +2,35 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/56521973?s=460&u=fdcfb7f2c627adc5acb808a00cb9994210d5d3ba&v=4",
+        name: "Felipe Schiavon",
+        role: "Instrutor - Rocketseat",
+        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="http://" target="_blank">Rocketseat</a>',
+        link: [
+            { name: "Github", url: "https://github.com/FehSchiavon" },
+            { name: "Twitter", url: "https://twitter.com/maykbrito?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" },
+            { name: "Linkedin", url: "https://www.linkedin.com/in/felipeschiavon/" }
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio")
+    return res.render("portfolio", { items: videos })
 })
 
 server.listen(5000, function() {
